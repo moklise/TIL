93eb64ead10fe87f6b52870c4600999a7673bda2
minseok <moklise@gmail.com>
2016-06-04 18:11:44 +0900

    [Modern C++] move sementics, variadic template, initializer_list

---
+++ C++/Modern Cpp/Modern Cpp/move sementics.cpp
@@ -0,0 +1,9 @@
+//
+//  move sementics.cpp
+//  Modern Cpp
+//
+//  Created by 민석 on 2016. 6. 1..
+//  Copyright © 2016년 민석. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ C++/Modern Cpp/Modern Cpp/variadic template.cpp
@@ -0,0 +1,9 @@
+//
+//  variadic template.cpp
+//  Modern Cpp
+//
+//  Created by 민석 on 2016. 6. 1..
+//  Copyright © 2016년 민석. All rights reserved.
+//
+
+#include <stdio.h>
